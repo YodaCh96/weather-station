1620c417dacbce34a9e26fb2b94f27cbb51194fb
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-03-22 07:43:43 +0100

    Update weather_station_01.m

--- weather_station_01.m
+++ weather_station_01.m
@@ -1,4 +1,4 @@
-% Exercise 1.1
+% Task 1.1
 % Import data from MET Office Weather Data
 opts = delimitedTextImportOptions("NumVariables", 8);
 
@@ -23,18 +23,18 @@ weatherD = readtable("C:\Users\Ioannis\Desktop\Github_Repos\MATLAB\Wetterstation
 % Clear temporary variables
 clear opts
 
-% Exercise 1.2
+% Task 1.2
 % Print summary of table
 summary(weatherD);
 
-% Exercise 1.3
+% Task 1.3
 % A new variable is created here, which contains only valid values
 wDfixed = rmmissing(weatherD);
 
 % The number of removed lines is printed here
 removedRows = height(weatherD) - height(wDfixed);
 
-% Exercise 1.4
+% Task 1.4
 % The table is sorted by year and month
 % sortedD = sortrows(wDfixed, [1 2]);            
 sortedD = sortrows(wDfixed, {'year', 'month'});
@@ -42,39 +42,39 @@ sortedD = sortrows(wDfixed, {'year', 'month'});
 % The maximal temperature over the years is displayed here.
 plot( sortedD.year, sortedD.tmax);
 
-% Exercise 1.5
+% Task 1.5
 groupSum = groupsummary(wDfixed, 'station');
 [~, index] = max(groupSum.GroupCount);
 maxStation = groupSum.station(index);
 maxStationD = wDfixed(wDfixed.station==maxStation,:);
 
-% Exercise 1.6
+% Task 1.6
 meanTemp = groupsummary( wDfixed, 'year', 'mean', {'tmax', 'tmin'}); 
 plot(meanTemp.year, meanTemp.mean_tmax, "-*");
 hold on;
 plot(meanTemp.year, meanTemp.mean_tmin, "-^" );
 
-% Exercise 1.7
+% Task 1.7
 % wDfixed.AvgTemp = (wDfixed.tmax + wDfixed.tmin)/2;
 medianTemp = groupsummary( wDfixed, 'year', 'median', {'tmax', 'tmin'}); 
 plot(medianTemp.year, medianTemp.median_tmax, "-+");    
 plot(medianTemp.year, medianTemp.median_tmin, "-<" );
 hold off;
 
-% Exercise 1.8
+% Task 1.8
 meanRainPerStation = groupsummary(wDfixed, {'station', 'year'}, 'mean', 'rain');
 [~, idxMin] = min( meanRainPerStation.mean_rain );
 [~, idxMax] = max( meanRainPerStation.mean_rain );
 minRainS = wDfixed.station(idxMin);
 maxRainS = wDfixed.station(idxMax);
 
-% Exercise 1.9
+% Task 1.9
 scatter(maxStationD.tmax, maxStationD.rain, 'filled');
 title('Scatterplot temperature vs amount of water')
 ylabel('amount of water')
 xlabel('temperature')
 
-% Exercise 1.10
+% Task 1.10
 minD = wDfixed(wDfixed.station == minRainS,:);
 maxD = wDfixed(wDfixed.station == maxRainS,:);
 ismember(minD, wDfixed);
