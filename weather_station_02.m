a28e14872e0c0866a47c58411ddfd209f2feab6d
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-03-22 07:44:33 +0100

    Update weather_station_02.m

--- weather_station_02.m
+++ weather_station_02.m
@@ -1,5 +1,4 @@
-%% Setup the Import Options and import the data
-
+% Setup the Import Options and import the data
 opts = delimitedTextImportOptions("NumVariables", 8);
 
 % Specify range and delimiter
@@ -20,35 +19,35 @@ opts = setvaropts(opts, ["af", "station"], "EmptyFieldRule", "auto");
 % Import the data
 weatherD = readtable("C:\Users\Ioannis\Desktop\Github_Repos\MATLAB\Wetterstation\MET_Office_Weather_Data.csv", opts);
 
-%% Clear temporary variables
+% Clear temporary variables
 clear opts
-%% cleanup NaN
 
+% cleanup NaN
 wDfixed = rmmissing(weatherD);
 removedRows = height(weatherD) - height(wDfixed);
 
-%% compute dataset of year 2020
+% compute dataset of year 2020
 data2020 = wDfixed(wDfixed.year==2020,:);
 test = categories(data2020.station)
 
-%% compute above/below median
+% compute above/below median
 aboveMedian = data2020(data2020.tmin > median(data2020.tmin),:)
 %belowMedian = data2020(data2020.tmin <= median(data2020.tmin),:)
 belowMedian = setdiff(data2020, aboveMedian)
 
 %intersect(aboveMedian, belowMedian)
 
-%% compute lower and upper quartile
+% compute lower and upper quartile
 upperQuartile = aboveMedian(aboveMedian.tmin <= median( aboveMedian.tmin ),:);
 aboveUpperQ = setdiff(aboveMedian, upperQuartile);
 lowerQuartile = belowMedian(belowMedian.tmin > median(belowMedian.tmin),:);
 belowLowerQ = setdiff(belowMedian, lowerQuartile);
 
-%% compute union of upper and lower quartile
+% compute union of upper and lower quartile
 box = union(upperQuartile, lowerQuartile);
 %stations = categories(box.station);
 stations = groupsummary(box, "station");
 
-%% compute the same using the diffset operation
+% compute the same using the diffset operation
 box2 = setdiff(setdiff(data2020, aboveUpperQ), belowLowerQ); 
 all(box2.station == box.station)
